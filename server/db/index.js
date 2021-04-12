9c2a14287f87f79f032540383bf6ed467775ad8a
clairehong11 <clairehong11@gmail.com>
2021-04-11 23:30:39 -0700

    updated db file with env variable

---
+++ server/db/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const uri = process.env.MONGODB_URI;
+
+mongoose
+    .connect(uri, { useNewUrlParser: true })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+
+const db = mongoose.connection
+
+module.exports = db
